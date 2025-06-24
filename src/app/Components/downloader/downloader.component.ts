0585b281ec392d4e91c30bbc090e372fab29a79a
choudharymahipal <mschoudhary81@gmail.com>
2025-06-24 22:42:32 +0530

    test app

---
+++ src/app/Components/downloader/downloader.component.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-downloader',
+  templateUrl: './downloader.component.html',
+  styleUrl: './downloader.component.scss'
+})
+export class DownloaderComponent {
+  url: string = '';
+  mediaData: any = null;
+  error: string = '';
+  loading: boolean = false;
+
+  constructor(private http: HttpClient) {}
+
+  downloadMedia() {
+    this.error = '';
+    this.mediaData = null;
+
+    if (!this.url || !this.isValidURL(this.url)) {
+      this.error = 'Please enter a valid URL.';
+      return;
+    }
+
+    this.loading = true;
+
+    this.http
+      .get<any>(`https://api.example.com/download?url=${encodeURIComponent(this.url)}`)
+      .subscribe({
+        next: (res) => {
+          this.loading = false;
+          if (res.success) {
+            this.mediaData = res.data;
+          } else {
+            this.error = res.message || 'Unable to download. The content might be private.';
+          }
+        },
+        error: (err) => {
+          this.loading = false;
+          this.error = 'Something went wrong or the URL is unsupported.';
+        }
+      });
+  }
+
+  isValidURL(url: string): boolean {
+    const pattern = /^(http|https):\/\//;
+    return pattern.test(url);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DownloaderComponent } from './Components/downloader/downloader.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DownloaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
